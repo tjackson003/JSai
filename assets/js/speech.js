4a9bc9a37b9eb5cfe5c6ff4dd15ba13715b7db07
TJ Jackson <elitefourfresh@gmail.com>
2019-08-24 21:45:17 -0400

    arrays show up in the console

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -7,7 +7,13 @@ let p = document.createElement("p");
 transcript_element.appendChild(p);
 
 recognition.addEventListener("result", (e) => {
-    console.log(e);
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript);
+
+    console.log(transcript);
 });
+
+recognition.addEventListener("end", recognition.start);
 
 recognition.start();
