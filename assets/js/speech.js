d2de7a1de9779697369e2ae1c8a506a6188563d7
TJ Jackson <elitefourfresh@gmail.com>
2019-08-24 22:01:27 -0400

    text shows up on separate lines; found out why chrome keeps asking me for permission

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -1,17 +1,26 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; // STARTS SPEECH RECOGNITION IF SUPPORTED BY THE BROWSER (AT THIS TIME, CHROME IS THE ONLY BROWSER THAT SUPPORTS)
 
+// VARIABLES
 const recognition = new SpeechRecognition();
+recognition.interimResults = true;
 const transcript_element = document.getElementById("transcript");
 
 let p = document.createElement("p");
 transcript_element.appendChild(p);
 
+// SPEECH RECOGNITION EVENT LISTENER (WILL REPEATEDLY ASK FOR PERMISSION BECAUSE PATH NOT SECURE "HTTPS")
 recognition.addEventListener("result", (e) => {
     const transcript = Array.from(e.results)
     .map(result => result[0])
     .map(result => result.transcript);
 
-    console.log(transcript);
+    p.textContent = transcript;
+    if(e.results[0].isFinal) {
+        p = document.createElement("p");
+        p.textContent = transcript;
+        transcript_element.appendChild(p);
+        p.textContent = "";
+    }
 });
 
 recognition.addEventListener("end", recognition.start);
